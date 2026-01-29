1ffa25e8c589c591535a53be0f2f4ec686b4da82
info-mate <sk88875@gmail.com>
2026-01-30 04:53:52 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "info-mates-v1";
+
+const FILES_TO_CACHE = [
+  "index.html",
+  "exam.html",
+  "rrb-test.html",
+  "manifest.json",
+  "logo.png",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
